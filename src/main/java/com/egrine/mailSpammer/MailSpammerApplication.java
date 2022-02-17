0c11cbf4a234f073d0eb22305fdf72929a8fc571
WDev-Beginner <76706155+WDev-Beginner@users.noreply.github.com>
2022-02-17 21:52:58 +0100

    initial project setup

---
+++ src/main/java/com/egrine/mailSpammer/MailSpammerApplication.java
@@ -0,0 +1,13 @@
+package com.egrine.mailSpammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSpammerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailSpammerApplication.class, args);
+	}
+
+}
