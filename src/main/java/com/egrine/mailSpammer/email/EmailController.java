29360d492245a7268a3e31dab244cccda71444ca
WDev-Beginner <76706155+WDev-Beginner@users.noreply.github.com>
2022-02-27 23:11:06 +0100

    implemented the rest api for the user templates skeleton code

--- src/main/java/com/egrine/mailSpammer/email/EmailController.java
+++ src/main/java/com/egrine/mailSpammer/email/EmailController.java
@@ -2,10 +2,50 @@ package com.egrine.mailSpammer.email;
 
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.annotation.ReadOnlyProperty;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController("/templates}")
 @AllArgsConstructor
 public class EmailController {
     private final EmailService service;
+
+    // user email template endpoints
+    // todo -> add some user session handling to handle these data requests.
+    // todo -> create exception classes to handle different api http responses
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("")
+    List<EmailTemplate> getAllUserTemplates(){
+        // return all the templates belonging to the logged-in user
+        return new ArrayList<>();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    EmailTemplate addUserTemplate(@RequestBody EmailTemplate newEmailTemplate){
+        // add the email to the user template list
+        return new EmailTemplate();
+    }
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PutMapping("")
+    EmailTemplate updateUserTemplate(@RequestBody EmailTemplate updatedEmailTemplate){
+        // update the email template here
+        return new EmailTemplate();
+    }
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @DeleteMapping("")
+    void deleteUserTemplate(){
+        // delete the email template here
+    }
+
+
+
+
 }
